b9c270c9f2d18e55487276f2db55279d53b79e76
codeeninja <102271798+codeeninja@users.noreply.github.com>
2023-11-01 16:47:12 +0530

    s

--- src/Gallery.js
+++ src/Gallery.js
@@ -5,68 +5,53 @@ import Picturecard from "./Picturecard";
 const Gallery = () => {
   const [category, setCategory] = useState("");
   const [images, setImages] = useState([]);
-  const [city,setCity]=useState('');
-  // const[cities,setcities]=(["London","Mumbai","HongKong","New York"]);
-  const cities=["London","Mumbai","HongKong","New York"]
-  const [searchResult,setsearchCities]=useState([]);
-  const searchCity=async(e)=>{
-    if(city){
+  const [city, setCity] = useState('');
+  const cities = ["London", "Mumbai", "HongKong", "New York"];
+  const [searchResult, setsearchCities] = useState([]);
+
+  const searchCity = async (e) => {
+    if (city) {
       const response = await axios.get(
         `https://api.unsplash.com/search/photos/?query=${city}&client_id=t4ccjeUgLYVfuN66h1iKdFfLdRGOV6-Ka6NLpoMotJM`
-      ).then(response=>{
+      ).then(response => {
         setsearchCities(response.data.results);
-      }).catch(error =>{
+      }).catch(error => {
         console.log(error);
       })
     }
   }
-  useEffect(()=>{
-    searchCity();
-  },[city])
 
-  // const searchImages = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       `https://api.unsplash.com/search/photos/?query=${category}&client_id=t4ccjeUgLYVfuN66h1iKdFfLdRGOV6-Ka6NLpoMotJM`
-  //     );
-  //     setImages(response.data.results);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-  const handelCityChange=(e)=>{
+  useEffect(() => {
+    searchCity();
+  }, [city])
+
+  const handelCityChange = (e) => {
     setCity(e.target.value);
   }
 
   return (
-    <>
-    <div className="gallery-container">
-      <h1 className="mb-4">Unsplash</h1>
-      <div className="search-bar">
-        <select value={city} onChange={handelCityChange}>
-          <option value="">Select a city</option>
-          {cities.map((city,index)=>(
-            <option key={index} value={city}>{city}</option>
-          ))}
-        </select>
-        {/* <button onClick={searchCity}>Search</button> */}
-        {/* <input
-          type="text"
-          placeholder="Enter a category name"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-        <button onClick={searchImages}>Search</button> */}
+    <div className="container">
+      <h1 className="mt-4">Unsplash</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="search-bar">
+            <select className="form-control" value={city} onChange={handelCityChange}>
+              <option value="">Select a city</option>
+              {cities.map((city, index) => (
+                <option key={index} value={city}>{city}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="row positionContainer">
+        {searchResult.map((result, index) => (
+          <div className="col-md-4" key={result.id}>
+            <Picturecard setsearchCities={setsearchCities} image={result} />
+          </div>
+        ))}
       </div>
     </div>
-    <div>
-      <div className="positionContainer">
-    {searchResult.map((result,index)=>(
-       <Picturecard key={result.id} image={result} />
-    ))}
-      </div>
-  </div>
-  </>
   );
 };
 
--- src/Picturecard.js
+++ src/Picturecard.js
@@ -1,32 +1,32 @@
 import React, { useState } from 'react';
 import './App.css';
 import axios from 'axios';
-const Picturecard = ({ image }) => {
+const Picturecard = ({ image,setsearchCities }) => {
   const [start, setStart] = useState('');
   const [end, setEnd] = useState('');
   const [word, setWord] = useState([]);
   const [Result, setResult] = useState([]);
-  
-  const handelSearch = async(e) => {
+  const handelSearch = async (e) => {
     if (start && end) {
-      const userInput = image.description.split(' ').slice(Number(start)-1 , Number(end));
+      const userInput = image.description.split(' ').slice(Number(start) - 1, Number(end));
       console.log(userInput)
       // console.log(userInput)
       setWord(e.target.value)
       let wordResult = [];
-     for(let word of userInput){
-        try{
-          const serverResp=await axios.get(`https://api.unsplash.com/search/photos/?query=${word}&client_id=t4ccjeUgLYVfuN66h1iKdFfLdRGOV6-Ka6NLpoMotJM`)
-          wordResult=serverResp.data.results;
+      for (let word of userInput) {
+        try {
+          const serverResp = await axios.get(`https://api.unsplash.com/search/photos/?query=${word}&client_id=t4ccjeUgLYVfuN66h1iKdFfLdRGOV6-Ka6NLpoMotJM`)
+          wordResult = serverResp.data.results;
+          setsearchCities(wordResult)
           console.log(wordResult);
         }
-        catch(error){
+        catch (error) {
           console.error(error)
         }
-        setResult(wordResult)
       }
     }
   }
+
 
   const handelstartChange = (e) => {
     setStart(e.target.value);
@@ -35,27 +35,45 @@ const Picturecard = ({ image }) => {
     setEnd(e.target.value);
   }
   return (
-    <>    <div className="image-card">
-      <img src={image.urls.regular} alt={image.description} />
-      <div className="image-info">
-        <p style={{ fontSize: "20px" }}>Author: {image.user.name}</p>
-        <p style={{ fontSize: "20px" }}>Description: {image.description || 'No description available'}</p>
-        <a className='btn btn-success' href={image.links.html} target="_blank" rel="noopener noreferrer">
-          View on Unsplash
-        </a>
+    <>
+      <div className="image-card">
+        <img src={image.urls.regular} alt={image.description} />
+        <div className="image-info">
+          <p style={{ fontSize: "20px" }}>Author: {image.user.name}</p>
+          <p style={{ fontSize: "20px" }}>Description: {image.description || 'No description available'}</p>
+          <a className='btn btn-success' href={image.links.html} target="_blank" rel="noopener noreferrer">
+            View on Unsplash
+          </a>
+        </div>
+        <div className="container">
+          <input type="text" placeholder='Enter Start position' value={start} onChange={handelstartChange} />
+          <input type="text" placeholder='Enter Start position' value={end} onChange={handelendChange} />
+          <button className='btn btn-success' onClick={handelSearch}>Search</button>
+        </div>
       </div>
-      <div className="container">
-        <input type="text" placeholder='Enter Start position' value={start} onChange={handelstartChange} />
-        <input type="text" placeholder='Enter Start position' value={end} onChange={handelendChange} />
-        <button className='btn btn-success' onClick={handelSearch}>Search</button>
-      </div>
-    </div>
-    <div className="result">
-       {Result.map((result) => (
-          <img key={result.id} src={result.urls.small} />
+      <div className="positionContainer ">
+        {Result.map((result) => (
+          <>
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <div className="secImgCard">
+
+                    <img key={result.id} src={result.urls.small} />
+                    <p className='username'>Author:{result.user.name}</p>
+                    <p className='resultDesc' style={{ fontSize: "20px" }}>Description:{result.description}</p>
+                    <a className='btn btn-success mb-2 ' href={result.links.html} target="_blank" rel="noopener noreferrer">
+                      View on Unsplash
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
         ))}
-    </div>
-  </>
+      </div>
+
+    </>
 
   );
 };
